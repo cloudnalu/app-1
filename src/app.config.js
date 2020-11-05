74a7c5acb9bbb101a436ad17513be71f5657f0d1
TJ Grist <tjgrst@gmail.com>
2020-11-04 16:28:12 -0800

    use values

--- src/app.config.js
+++ src/app.config.js
@@ -2,11 +2,12 @@ const issuer = process.env.ISSUER;
 const redirectUri = process.env.REDIRECT_URI;
 const clientId = process.env.CLIENT_ID;
 
+console.log('env', issuer, redirectUri, clientId)
 export default {
-    url: issuer,
-    issuer: `${issuer}/oauth2/default`,
-    redirect_uri: redirectUri,
-    client_id: clientId,
+    url: `https://cloudnalu.oktapreview.com`,
+    issuer: `https://cloudnalu.oktapreview.com/oauth2/default`,
+    redirect_uri: `http://localhost:3000/implicit/callback`,
+    client_id: `0oa4ls58zN5pBW1hp1d6`,
     scopes: ['openid', 'profile', 'email'],
     pkce: true
   };
