ff13ae40775a3a7f142996cf8b7e178e959cd004
TJ Grist <tjgrst@gmail.com>
2020-11-04 16:04:58 -0800

    set env

--- src/app.config.js
+++ src/app.config.js
@@ -1,3 +1,12 @@
-export default {
+const issuer = process.env.ISSUER;
+const redirectUri = process.env.REDIRECT_URI;
+const clientId = process.env.CLIENT_ID;
 
+export default {
+    url: issuer,
+    issuer: `${issuer}/oauth2/default`,
+    redirect_uri: redirectUri,
+    client_id: clientId,
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true
   };
