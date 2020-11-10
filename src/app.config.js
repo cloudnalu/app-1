897edde58455a16eadab44eeb87440c3eb75874d
tjgrist <tjgrst@gmail.com>
2020-11-09 22:03:18 -0800

    update config

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@ const prodConfig = {
   url: oktapreview,
   issuer: `${oktapreview}/oauth2/default`,
   redirect_uri: `${window.location.origin}/implicit/callback`,
-  client_id: `0oa4ls58zN5pBW1hp1d6`,
+  client_id: `0oa4op9istUMy3BcP1d6`,
   scopes: ['openid', 'profile', 'email'],
   pkce: true
 };
